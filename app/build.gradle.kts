86007a930fa2edb24ef3c10d027b0e7bc3ffa72d
test <test@example.com>
2026-05-16 08:46:45 +0000

    Fix: Update Compose and dependencies versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.7"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packagingOptions {
@@ -61,11 +61,12 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
 
     // JSON parsing
     implementation("com.google.code.gson:gson:2.10.1")
